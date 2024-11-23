880d9e0a5d9adaf2640e2a5cc35cc5ef47055107
AMihkel <andre.mihkel3@gmail.com>
2024-11-23 20:29:57 +0200

    B osa algus
    sisse logimise ja v2lja logimise v6imalus on loodud aga lehed on k6ik tyhjad

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (!isset($_SESSION['tuvastamine'])) {
+  header('Location: login.php');
+  exit();
+  }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+  
+  <h1>Salajane info</h1>
+  <p>Salainfo</p>
+  <form action="logout.php" method="post">
+	<input type="submit" value="Logi välja" name="logout">
+</form>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
